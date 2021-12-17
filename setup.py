2e9b81f84cd51410384908fc267d3e6646f4d394
pabloatarama <pabloatarama@gmail.com>
2021-12-17 14:37:40 -0500

    2021.12.17

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(name="series-de-tiempo-pabloatarama",
-      version="2021.12.1",
+      version="2021.12.17",
       author="Pablo Atarama",
       author_email="contacto@pabloatarama.com",
       description="Modelos para el análisis de series de tiempo y proyecciones.",
       long_description=long_description,
-      long_description_content_type="text/markdown",      
+      long_description_content_type="text/markdown",
       url="https://pabloatarama.com/SeriesDeTiempo/",
       packages=["SeriesDeTiempo"],
       )
